b3216cee3ed6ea9b4f4b75b8f088e27b7bb08649
Joel <waylaidwanderer@live.com>
2018-02-23 11:02:16 -0800

    Allow settings different host/port/password for redis

--- settings.example.js
+++ settings.example.js
@@ -5,4 +5,9 @@ module.exports = {
     accounts: [
         'API key here',
     ],
+    redis: {
+        host: '127.0.0.1',
+        port: 6379,
+        password: 'remove this line completely if no password is set',
+    },
 };
